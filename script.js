5f053966147ffba10f007e650114ebaff07ba3f2
damian.deleon <damianmdeleon@gmail.com>
2021-01-29 14:37:34 -0600

    started fetch data function from API

--- script.js
+++ script.js
@@ -28,6 +28,26 @@ var formSubmitHandler = function (event) {
 // create function that will insert the city search into the API search dynamically
 var getCityWeather = function(city){
     var cityUrl = "api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d26847a740a8421604e3803e540bf50a"
-}
 
+    console.log(cityUrl);
+
+    fetch(cityUrl)
+        .then(function (response) {
+            if(response.ok) {
+                response.json()
+                .then(function (data) {
+                    displayWeather(data.items)
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        });
+    }
+//create function to actually display weather into a user friendly presentation from the API code.  **** Call the funciton displayWeather() *****
+
+function displayWeather(data) {
+    console.log(data);
+};
+
+//create city search event listener
 cityFormEl.addEventListener('submit', formSubmitHandler);

6bf25c125437d7c500eca0a20d2942c693059b2a
damian.deleon <damianmdeleon@gmail.com>
2021-01-29 15:48:25 -0600

    added a zip serach function as well

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ var cityTempResults = document.querySelector(".cityTempResults");
 var cityHumidityResults = document.querySelector(".cityHumidityResults");
 var cityWindSpeedResults = document.querySelector(".cityWindSpeedResults");
 var cityUVIndexResults = document.querySelector(".cityUVIndexResults");
+var zipFormEl = document.querySelector("#zip-form");
+var zipCitySearchInput = document.querySelector("#zipCitySearchInput");
 
 
 
 // create form submit handler to take out the whitespace and review what was entered. 
-var formSubmitHandler = function (event) {
+var cityFormSubmitHandler = function (event) {
     event.preventDefault();
   
     var cityname = citySearchInput.value.trim();
@@ -22,15 +24,50 @@ var formSubmitHandler = function (event) {
       cityNameResults.textContent = cityname + " (make code to get today's date)";
       citySearchInput.value = '';
     } else {
-      alert('Please enter a GitHub username');
+      alert('Please enter a city name before searching');
     }
   };
 
 // create function that will insert the city search into the API search dynamically
 var getCityWeather = function(city){
+    console.log(typeof city);
     var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=d26847a740a8421604e3803e540bf50a"
 
     fetch(cityUrl)
+        .then(function (response) {
+            if(response.ok) {
+                response.json()
+                .then(function (data) {
+                    displayWeather(data)
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        });
+    }
+
+// create form submit handler to take out the whitespace and review what was entered. 
+var zipFormSubmitHandler = function (event) {
+    event.preventDefault();
+  
+    var cityzip = zipCitySearchInput.value.trim();
+  
+    if (cityzip) {
+      getZIPWeather(cityzip);
+  
+      cityNameResults.textContent = cityzip + " (make code to get today's date)";
+      citySearchInput.value = '';
+    } else {
+      alert('Please enter a Zip Code before searching');
+    }
+  };
+
+// create function that will insert the city search into the API search dynamically
+var getZIPWeather = function(zip){
+    console.log(typeof zip);
+    var zipUrl = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+"&units=imperial&appid=d26847a740a8421604e3803e540bf50a"
+
+    fetch(zipUrl)
         .then(function (response) {
             if(response.ok) {
                 response.json()
@@ -56,4 +93,5 @@ function displayWeather(data) {
 };
 
 //create city search event listener
-cityFormEl.addEventListener('submit', formSubmitHandler);
+cityFormEl.addEventListener('submit', cityFormSubmitHandler);
+zipFormEl.addEventListener('submit', zipFormSubmitHandler);

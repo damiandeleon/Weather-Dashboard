35e818e4c3b420ad131ef6052c28c4280bdd8c9f
damian.deleon <damianmdeleon@gmail.com>
2021-01-31 12:04:21 -0600

    updated code to run UV index indicator

--- script.js
+++ script.js
@@ -9,89 +9,121 @@ var cityWindSpeedResults = document.querySelector(".cityWindSpeedResults");
 var cityUVIndexResults = document.querySelector(".cityUVIndexResults");
 var zipFormEl = document.querySelector("#zip-form");
 var zipCitySearchInput = document.querySelector("#zipCitySearchInput");
+var UVReader = document.querySelector(".UVreader");
 
 
 
 // create form submit handler to take out the whitespace and review what was entered. 
 var cityFormSubmitHandler = function (event) {
     event.preventDefault();
-  
+
     var cityname = citySearchInput.value.trim();
-  
+
     if (cityname) {
-      getCityWeather(cityname);
-  
-      cityNameResults.textContent = cityname + " (make code to get today's date)";
-      citySearchInput.value = '';
+        getCityWeather(cityname);
+
+
+        cityNameResults.textContent = cityname + " (make code to get today's date)";
+        citySearchInput.value = '';
     } else {
-      alert('Please enter a city name before searching');
+        alert('Please enter a city name before searching');
     }
-  };
+};
 
 // create function that will insert the city search into the API search dynamically
-var getCityWeather = function(city){
-    console.log(typeof city);
-    var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=d26847a740a8421604e3803e540bf50a"
+var getCityWeather = function (city) {
+    var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=d26847a740a8421604e3803e540bf50a"
 
     fetch(cityUrl)
         .then(function (response) {
-            if(response.ok) {
+            if (response.ok) {
                 response.json()
-                .then(function (data) {
-                    displayWeather(data)
-                });
+                    .then(function (data) {
+                        displayWeather(data)
+                        getUVIndex(data);
+                    });
             } else {
                 alert("Error: " + response.statusText);
             }
         });
-    }
+}
 
 // create form submit handler to take out the whitespace and review what was entered. 
-var zipFormSubmitHandler = function (event) {
-    event.preventDefault();
-  
-    var cityzip = zipCitySearchInput.value.trim();
-  
-    if (cityzip) {
-      getZIPWeather(cityzip);
-  
-      cityNameResults.textContent = cityzip + " (make code to get today's date)";
-      zipCitySearchInput.value = '';
-    } else {
-      alert('Please enter a Zip Code before searching');
-    }
-  };
+// var zipFormSubmitHandler = function (event) {
+//     event.preventDefault();
 
-// create function that will insert the city search into the API search dynamically
-var getZIPWeather = function(zip){
-    console.log(typeof zip);
-    var zipUrl = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+"&units=imperial&appid=d26847a740a8421604e3803e540bf50a"
+//     var cityzip = zipCitySearchInput.value.trim();
 
-    fetch(zipUrl)
-        .then(function (response) {
-            if(response.ok) {
-                response.json()
-                .then(function (data) {
-                    displayWeather(data)
-                });
-            } else {
-                alert("Error: " + response.statusText);
-            }
-        });
-    }
+//     if (cityzip) {
+//       getZIPWeather(cityzip);
+
+//       cityNameResults.textContent = cityzip + " (make code to get today's date)";
+//       zipCitySearchInput.value = '';
+//     } else {
+//       alert('Please enter a Zip Code before searching');
+//     }
+//   };
+
+// // create function that will insert the city search into the API search dynamically
+// var getZIPWeather = function(zip){
+//     var zipUrl = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+"&units=imperial&appid=d26847a740a8421604e3803e540bf50a"
+
+//     fetch(zipUrl)
+//         .then(function (response) {
+//             if(response.ok) {
+//                 response.json()
+//                 .then(function (data) {
+//                     displayWeather(data)
+//                 });
+//             } else {
+//                 alert("Error: " + response.statusText);
+//             }
+//         });
+//     }
 //create function to actually display weather into a user friendly presentation from the API code.  **** Call the funciton displayWeather() *****
 
 function displayWeather(data) {
-    // for (var i = 0; i < data.length; i++){
-        var temperature = data.main.temp;
-        cityTempResults.textContent = "Temperature: " +temperature +" ° F";
-        var humidity = data.main.humidity;
-        cityHumidityResults.textContent = "Humidity: "+humidity +"%";
-        windSpeed = data.wind.speed;
-        cityWindSpeedResults.textContent = "Wind Speed: " +windSpeed + " MPH";
-    // }
+    var temperature = data.main.temp;
+    cityTempResults.textContent = "Temperature: " + temperature + " ° F";
+    var humidity = data.main.humidity;
+    cityHumidityResults.textContent = "Humidity: " + humidity + "%";
+    windSpeed = data.wind.speed;
+    cityWindSpeedResults.textContent = "Wind Speed: " + windSpeed + " MPH";
 };
+
+function getUVIndex(data1) {
+    var latitidue = data1.coord.lat;
+    var longitude = data1.coord.lon;
+    var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitidue + "&lon=" + longitude + "&appid=d26847a740a8421604e3803e540bf50a"
+
+    fetch(uvUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (data1) {
+                        displayUVIndex(data1);
+                    });
+            }
+        });
+    function displayUVIndex(data1) {
+        var UVIndex = data1.value;
+        UVReader.textContent = UVIndex;
+        if (UVIndex > 0 && UVIndex <=2.99) {
+            UVReader.setAttribute("style", "background-color: green; color: white");
+        } else if (UVIndex > 2.99 && UVIndex <=5.99) {
+            UVReader.setAttribute("style", "background-color: yellow; color: black");
+        } else if (UVIndex > 5.99 && UVIndex <=7.99) {
+            UVReader.setAttribute("style", "background-color: darkorange; color: white");
+        } else if (UVIndex > 7.99 && UVIndex <=10.99) {
+            UVReader.setAttribute("style", "background-color: red; color: white");
+        } else if (UVIndex > 10.99) {
+            UVReader.setAttribute("style", "background-color: purple; color: white");
+        }
+    };
+}
+
+
 
 //create city search event listener
 cityFormEl.addEventListener('submit', cityFormSubmitHandler);
-zipFormEl.addEventListener('submit', zipFormSubmitHandler);
+// zipFormEl.addEventListener('submit', zipFormSubmitHandler);

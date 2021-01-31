71f71cf5da207988fe6a79ef7be7276ea1bca85d
damian.deleon <damianmdeleon@gmail.com>
2021-01-31 13:57:06 -0600

    updated code for the 5 day forecast

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ var cityUVIndexResults = document.querySelector(".cityUVIndexResults");
 var zipFormEl = document.querySelector("#zip-form");
 var zipCitySearchInput = document.querySelector("#zipCitySearchInput");
 var UVReader = document.querySelector(".UVreader");
+var temptodayplus1 = document.querySelector("#temptodayplus1");
+var humiditytodayplus1 = document.querySelector("#humiditytodayplus1");
+var temptodayplus2 = document.querySelector("#temptodayplus2");
+var humiditytodayplus2 = document.querySelector("#humiditytodayplus2");
+var temptodayplus3 = document.querySelector("#temptodayplus3");
+var humiditytodayplus3 = document.querySelector("#humiditytodayplus3");
+var temptodayplus4 = document.querySelector("#temptodayplus4");
+var humiditytodayplus4 = document.querySelector("#humiditytodayplus4");
+var temptodayplus5 = document.querySelector("#temptodayplus5");
+var humiditytodayplus5 = document.querySelector("#humiditytodayplus5");
 
 
 
@@ -21,6 +31,7 @@ var cityFormSubmitHandler = function (event) {
 
     if (cityname) {
         getCityWeather(cityname);
+        getFiveDayForecast(cityname);
 
 
         cityNameResults.textContent = cityname + " (make code to get today's date)";
@@ -48,38 +59,6 @@ var getCityWeather = function (city) {
         });
 }
 
-// create form submit handler to take out the whitespace and review what was entered. 
-// var zipFormSubmitHandler = function (event) {
-//     event.preventDefault();
-
-//     var cityzip = zipCitySearchInput.value.trim();
-
-//     if (cityzip) {
-//       getZIPWeather(cityzip);
-
-//       cityNameResults.textContent = cityzip + " (make code to get today's date)";
-//       zipCitySearchInput.value = '';
-//     } else {
-//       alert('Please enter a Zip Code before searching');
-//     }
-//   };
-
-// // create function that will insert the city search into the API search dynamically
-// var getZIPWeather = function(zip){
-//     var zipUrl = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+"&units=imperial&appid=d26847a740a8421604e3803e540bf50a"
-
-//     fetch(zipUrl)
-//         .then(function (response) {
-//             if(response.ok) {
-//                 response.json()
-//                 .then(function (data) {
-//                     displayWeather(data)
-//                 });
-//             } else {
-//                 alert("Error: " + response.statusText);
-//             }
-//         });
-//     }
 //create function to actually display weather into a user friendly presentation from the API code.  **** Call the funciton displayWeather() *****
 
 function displayWeather(data) {
@@ -94,7 +73,7 @@ function displayWeather(data) {
 function getUVIndex(data1) {
     var latitidue = data1.coord.lat;
     var longitude = data1.coord.lon;
-    var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitidue + "&lon=" + longitude + "&appid=d26847a740a8421604e3803e540bf50a"
+    var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitidue + "&lon=" + longitude + "&appid=d26847a740a8421604e3803e540bf50a"
 
     fetch(uvUrl)
         .then(function (response) {
@@ -108,18 +87,48 @@ function getUVIndex(data1) {
     function displayUVIndex(data1) {
         var UVIndex = data1.value;
         UVReader.textContent = UVIndex;
-        if (UVIndex > 0 && UVIndex <=2.99) {
+        if (UVIndex > 0 && UVIndex <= 2.99) {
             UVReader.setAttribute("style", "background-color: green; color: white");
-        } else if (UVIndex > 2.99 && UVIndex <=5.99) {
+        } else if (UVIndex > 2.99 && UVIndex <= 5.99) {
             UVReader.setAttribute("style", "background-color: yellow; color: black");
-        } else if (UVIndex > 5.99 && UVIndex <=7.99) {
+        } else if (UVIndex > 5.99 && UVIndex <= 7.99) {
             UVReader.setAttribute("style", "background-color: darkorange; color: white");
-        } else if (UVIndex > 7.99 && UVIndex <=10.99) {
+        } else if (UVIndex > 7.99 && UVIndex <= 10.99) {
             UVReader.setAttribute("style", "background-color: red; color: white");
         } else if (UVIndex > 10.99) {
             UVReader.setAttribute("style", "background-color: purple; color: white");
         }
-    };
+    };  
+}
+function getFiveDayForecast(cityname){
+    var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=d26847a740a8421604e3803e540bf50a"
+
+    fetch(fiveDayUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        displayFiveDayForecast(data);
+                        console.log(data);
+                        temptodayplus1.textContent = data.list[1].main.temp_max;
+                        humiditytodayplus1.textContent = data.list[1].main.humidity;
+                        temptodayplus2.textContent = data.list[9].main.temp_max;
+                        humiditytodayplus2.textContent = data.list[9].main.humidity;
+                        temptodayplus3.textContent = data.list[17].main.temp_max;
+                        humiditytodayplus3.textContent = data.list[17].main.humidity;
+                        temptodayplus4.textContent = data.list[25].main.temp_max;
+                        humiditytodayplus4.textContent = data.list[25].main.humidity;
+                        temptodayplus5.textContent = data.list[33].main.temp_max;
+                        humiditytodayplus5.textContent = data.list[33].main.humidity;
+                    });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        });
+}
+
+function displayFiveDayForecast(data){
+
 }
 
 

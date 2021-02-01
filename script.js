8ed00514b78dc839e28f641af93fe076f75d4c4f
damian.deleon <damianmdeleon@gmail.com>
2021-02-01 08:43:52 -0600

    added weather icon funcationality to the 5 day forecast

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ var getCityWeather = function (city) {
             if (response.ok) {
                 response.json()
                     .then(function (data) {
+                        console.log("current weather------", data);
                         displayWeather(data)
                         getUVIndex(data);
                     });
@@ -110,23 +111,54 @@ function getFiveDayForecast(cityname){
                 response.json()
                     .then(function (data) {
                         displayFiveDayForecast(data);
-                        console.log(data);
+                        console.log("five day forecast------", data);
                         temptodayplus1.textContent = data.list[1].main.temp_max;
                         humiditytodayplus1.textContent = data.list[1].main.humidity;
-                        temptodayplus2.textContent = data.list[9].main.temp_max;
-                        humiditytodayplus2.textContent = data.list[9].main.humidity;
-                        temptodayplus3.textContent = data.list[17].main.temp_max;
-                        humiditytodayplus3.textContent = data.list[17].main.humidity;
-                        temptodayplus4.textContent = data.list[25].main.temp_max;
-                        humiditytodayplus4.textContent = data.list[25].main.humidity;
-                        temptodayplus5.textContent = data.list[33].main.temp_max;
-                        humiditytodayplus5.textContent = data.list[33].main.humidity;
-
-                        //add code to add pictures to class="imgtodayplus(x)" -----------
+                        var weatherIconCode1=data.list[1].weather[0].icon;
+                        console.log("Icon Code ----", weatherIconCode1);
                         var imgEL = document.createElement("img");
                         imgEL.setAttribute("width", "50px");
-                        imgEL.setAttribute("src", "http://openweathermap.org/img/wn/10d@2x.png");
+                        imgEL.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode1+"@2x.png");
+                        imgtodayplus1.append(imgEL);
+                        
+                        temptodayplus2.textContent = data.list[9].main.temp_max;
+                        humiditytodayplus2.textContent = data.list[9].main.humidity;
+                        var weatherIconCode2=data.list[9].weather[0].icon;
+                        console.log("Icon Code ----", weatherIconCode1);
+                        var imgEL = document.createElement("img");
+                        imgEL.setAttribute("width", "50px");
+                        imgEL.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode2+"@2x.png");
                         imgtodayplus2.append(imgEL);
+
+                        temptodayplus3.textContent = data.list[17].main.temp_max;
+                        humiditytodayplus3.textContent = data.list[17].main.humidity;
+                        var weatherIconCode3=data.list[17].weather[0].icon;
+                        console.log("Icon Code ----", weatherIconCode1);
+                        var imgEL = document.createElement("img");
+                        imgEL.setAttribute("width", "50px");
+                        imgEL.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode3+"@2x.png");
+                        imgtodayplus3.append(imgEL);
+
+                        temptodayplus4.textContent = data.list[25].main.temp_max;
+                        humiditytodayplus4.textContent = data.list[25].main.humidity;
+                        var weatherIconCode4=data.list[25].weather[0].icon;
+                        console.log("Icon Code ----", weatherIconCode1);
+                        var imgEL = document.createElement("img");
+                        imgEL.setAttribute("width", "50px");
+                        imgEL.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode4+"@2x.png");
+                        imgtodayplus4.append(imgEL);
+
+                        temptodayplus5.textContent = data.list[33].main.temp_max;
+                        humiditytodayplus5.textContent = data.list[33].main.humidity;
+                        var weatherIconCode5=data.list[33].weather[0].icon;
+                        console.log("Icon Code ----", weatherIconCode1);
+                        var imgEL = document.createElement("img");
+                        imgEL.setAttribute("width", "50px");
+                        imgEL.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode5+"@2x.png");
+                        imgtodayplus5.append(imgEL);
+
+                        //add code to add pictures to class="imgtodayplus(x)" -----------
+
                         
                     });
             } else {

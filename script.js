2a512801264c0dc48d0512496b1e6041ff4d4779
damian.deleon <damianmdeleon@gmail.com>
2021-02-02 15:30:29 -0600

    finalized code for local storage

--- script.js
+++ script.js
@@ -20,18 +20,75 @@ var temptodayplus4 = document.querySelector("#temptodayplus4");
 var humiditytodayplus4 = document.querySelector("#humiditytodayplus4");
 var temptodayplus5 = document.querySelector("#temptodayplus5");
 var humiditytodayplus5 = document.querySelector("#humiditytodayplus5");
-var listGroup = document.querySelector(".cityNameSearchItem");
+var cityNameSearchItem = document.querySelector(".cityNameSearchItem");
 var itemPlace1 = document.querySelector("#itemPlace1")
 var itemPlace2 = document.querySelector("#itemPlace2")
 var itemPlace3 = document.querySelector("#itemPlace3")
 var itemPlace4 = document.querySelector("#itemPlace4")
 var itemPlace5 = document.querySelector("#itemPlace5")
-var cityList = [];
+var cityList = "0"
 
 // create form submit handler to take out the whitespace and review what was entered. 
+renderLocalStorage();
+function renderLocalStorage() {
+    var renderedCity1 = localStorage.getItem("1");
+    if (renderedCity1 !== " ") {
+        var liEl = document.createElement("li");
+        liEl.setAttribute("class", "list-group-item");
+        liEl.setAttribute("id", "itemPlace");
+        liEl.textContent = renderedCity1;
+        cityNameSearchItem.appendChild(liEl);
+    } else { return };
 
-function renderLocalStorage(){
-console.log("second city List Run----", cityList);
+    var renderedCity2 = localStorage.getItem("2");
+    if (renderedCity2 !== " ") {
+        var liEl = document.createElement("li");
+        liEl.setAttribute("class", "list-group-item");
+        liEl.setAttribute("id", "itemPlace");
+        liEl.textContent = renderedCity2;
+        cityNameSearchItem.appendChild(liEl);
+    } else { return };
+
+    var renderedCity3 = localStorage.getItem("3");
+    if (renderedCity3) {
+        var liEl = document.createElement("li");
+        liEl.setAttribute("class", "list-group-item");
+        liEl.setAttribute("id", "itemPlace");
+        liEl.textContent = renderedCity3;
+        cityNameSearchItem.appendChild(liEl);
+    } else { return };
+
+    var renderedCity4 = localStorage.getItem("4");
+    if (renderedCity4) {
+        var liEl = document.createElement("li");
+        liEl.setAttribute("class", "list-group-item");
+        liEl.setAttribute("id", "itemPlace");
+        liEl.textContent = renderedCity4;
+        cityNameSearchItem.appendChild(liEl);
+    } else { return };
+
+    var renderedCity5 = localStorage.getItem("5");
+    if (renderedCity5) {
+        var liEl = document.createElement("li");
+        liEl.setAttribute("class", "list-group-item");
+        liEl.setAttribute("id", "itemPlace");
+        liEl.textContent = renderedCity5;
+        cityNameSearchItem.appendChild(liEl);
+    } else { return };
+
+
+}
+
+//got this from "https://www.youtube.com/watch?v=RbfG7NLKDgQ" How to store A JavaScript Array in Local Storage
+function saveLocalStorage(city) {
+    cityList++;
+    localStorage.setItem(cityList, city);
+    var liEl = document.createElement("li");
+    liEl.setAttribute("class", "list-group-item");
+    liEl.setAttribute("id", "itemPlace");
+    liEl.textContent = city;
+    cityNameSearchItem.appendChild(liEl);
+    return cityList
 }
 
 var cityFormSubmitHandler = function (event) {
@@ -41,14 +98,15 @@ var cityFormSubmitHandler = function (event) {
     if (cityname) {
         getCityWeather(cityname);
         getFiveDayForecast(cityname);
+        saveLocalStorage(cityname);
 
-        cityNameResults.textContent = cityname + "  (" + moment().format('l')+")"; 
+        cityNameResults.textContent = cityname + "  (" + moment().format('l') + ")";
         citySearchInput.value = '';
         cityList.value = cityname;
     } else {
         alert('Please enter a city name before searching');
     }
-     
+
 };
 
 
@@ -76,7 +134,7 @@ function displayWeather(data) {
     var weatherIconCode = data.weather[0].icon;
     var addIcon = document.createElement("img");
     addIcon.setAttribute("width", "50px");
-    addIcon.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode+"@2x.png");
+    addIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIconCode + "@2x.png");
     cityNameResults.append(addIcon);
     var temperature = data.main.temp;
     cityTempResults.textContent = "Temperature: " + temperature + " ° F";
@@ -114,9 +172,9 @@ function getUVIndex(data1) {
         } else if (UVIndex > 10.99) {
             UVReader.setAttribute("style", "background-color: purple; color: white");
         }
-    };  
+    };
 }
-function getFiveDayForecast(cityname){
+function getFiveDayForecast(cityname) {
     var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=d26847a740a8421604e3803e540bf50a"
 
     fetch(fiveDayUrl)
@@ -124,48 +182,48 @@ function getFiveDayForecast(cityname){
             if (response.ok) {
                 response.json()
                     .then(function (data) {
-                        if(todayplus1.imgEL){
+                        if (todayplus1.imgEL) {
                             todayplus1.removeChild(imgEL);
                         }
-                        todayplus1.textContent = moment().add(1, "days").format('l');  
+                        todayplus1.textContent = moment().add(1, "days").format('l');
                         temptodayplus1.textContent = data.list[1].main.temp_max;
                         humiditytodayplus1.textContent = data.list[1].main.humidity;
-                        var weatherIconCode1=data.list[1].weather[0].icon;
+                        var weatherIconCode1 = data.list[1].weather[0].icon;
                         //add code to add pictures to class="imgtodayplus(x)" -----------
                         imgtodayplus1.setAttribute("width", "50px");
-                        imgtodayplus1.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode1+"@2x.png");                        
-                        
-                        todayplus2.textContent = moment().add(2, "days").format('l'); 
+                        imgtodayplus1.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIconCode1 + "@2x.png");
+
+                        todayplus2.textContent = moment().add(2, "days").format('l');
                         temptodayplus2.textContent = data.list[9].main.temp_max;
                         humiditytodayplus2.textContent = data.list[9].main.humidity;
-                        var weatherIconCode2=data.list[9].weather[0].icon;
+                        var weatherIconCode2 = data.list[9].weather[0].icon;
                         //add code to add pictures to class="imgtodayplus(x)" -----------
                         imgtodayplus2.setAttribute("width", "50px");
-                        imgtodayplus2.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode2+"@2x.png");
+                        imgtodayplus2.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIconCode2 + "@2x.png");
 
-                        todayplus3.textContent = moment().add(3, "days").format('l'); 
+                        todayplus3.textContent = moment().add(3, "days").format('l');
                         temptodayplus3.textContent = data.list[17].main.temp_max;
                         humiditytodayplus3.textContent = data.list[17].main.humidity;
                         //add code to add pictures to class="imgtodayplus(x)" -----------
-                        var weatherIconCode3=data.list[17].weather[0].icon;
+                        var weatherIconCode3 = data.list[17].weather[0].icon;
                         imgtodayplus3.setAttribute("width", "50px");
-                        imgtodayplus3.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode3+"@2x.png");
+                        imgtodayplus3.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIconCode3 + "@2x.png");
 
-                        todayplus4.textContent = moment().add(4, "days").format('l'); 
+                        todayplus4.textContent = moment().add(4, "days").format('l');
                         temptodayplus4.textContent = data.list[25].main.temp_max;
                         humiditytodayplus4.textContent = data.list[25].main.humidity;
-                        var weatherIconCode4=data.list[25].weather[0].icon;
+                        var weatherIconCode4 = data.list[25].weather[0].icon;
                         //add code to add pictures to class="imgtodayplus(x)" -----------
                         imgtodayplus4.setAttribute("width", "50px");
-                        imgtodayplus4.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode4+"@2x.png");
+                        imgtodayplus4.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIconCode4 + "@2x.png");
 
-                        todayplus5.textContent = moment().add(5, "days").format('l'); 
+                        todayplus5.textContent = moment().add(5, "days").format('l');
                         temptodayplus5.textContent = data.list[33].main.temp_max;
                         humiditytodayplus5.textContent = data.list[33].main.humidity;
-                        var weatherIconCode5=data.list[33].weather[0].icon;
+                        var weatherIconCode5 = data.list[33].weather[0].icon;
                         //add code to add pictures to class="imgtodayplus(x)" -----------
                         imgtodayplus5.setAttribute("width", "50px");
-                        imgtodayplus5.setAttribute("src", "http://openweathermap.org/img/wn/"+weatherIconCode5+"@2x.png");
+                        imgtodayplus5.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIconCode5 + "@2x.png");
                     });
             } else {
                 alert("Error: " + response.statusText);
